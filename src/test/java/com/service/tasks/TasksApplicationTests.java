5b1b5d079f00a4a128c79313ff400d30bfc0da31
Henrique Pinto <pintohen2003@gmail.com>
2025-03-08 10:21:16 +0000

    Initial Commit

---
+++ src/test/java/com/service/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
